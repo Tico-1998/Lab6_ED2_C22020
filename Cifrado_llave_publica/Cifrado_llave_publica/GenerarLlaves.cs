06e75a5a26c2413a3323ab18dbac5d8b168415ec
Carlos Andrés Vargas Orué <voca98@hotmail.com>
2020-11-15 00:23:38 -0600

    Correcciones claves
    Se realizaron pequeñas correcciones y se guarda en .zip

--- Cifrado_llave_publica/Cifrado_llave_publica/GenerarLlaves.cs
+++ Cifrado_llave_publica/Cifrado_llave_publica/GenerarLlaves.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Security.Permissions;
 using System.Text;
 
 namespace Cifrado_llave_publica
 {
-    class GenerarLlaves
+    public class GenerarLlaves
     {
-        public void Calculos(int p, int q)//revisar toda la clase 
+        public void Calculos(ref int p,ref int q)
         {
             int n = p * q;
             int phi = (p - 1) * (q - 1);
             int e = ElegirE(phi);
             int d = EncontrarD(e, phi);
             string path = Environment.CurrentDirectory;
+            path = path + "\\clave";
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
             using (var file = new FileStream(path + "\\public.key",FileMode.Append))
             {
                 using(var writer=new BinaryWriter(file))
                 {
                     writer.Write(n + "," + e);
-                }
+                }                
             }
+            
             using (var file = new FileStream(path + "\\private.key", FileMode.Append))
             {
                 using (var writer = new BinaryWriter(file))
@@ -28,17 +36,22 @@ namespace Cifrado_llave_publica
                     writer.Write(n + "," + d);
                 }
             }
-            //GENERAR ZIP
+            string startPath = path;
+            string zipPath = path+".zip";
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+
+
         }
 
-        public int ElegirE(int phi)
+        private int ElegirE(int phi)
         {
             int[] primos = new int[167];
-            string path = Environment.CurrentDirectory;
-            path = path + "\\primos.txt";
-            string numeros = Convert.ToString(File.ReadLines(path));
+            string path = "C:\\Users\\voca9\\Desktop\\Cifrado_llave_publica\\Cifrado_llave_publica\\primos.txt"; 
+            //path = path + "\\primos.txt";
+            var numeros = File.ReadAllText(path);
             string [] numero = numeros.Split(',');
-            for (int i = 0; i < 167; i++)
+            for (int i = 0; i < numero.Length; i++)
             {
                 primos[i] = Convert.ToInt32(numero[i]);
             }
@@ -47,10 +60,11 @@ namespace Cifrado_llave_publica
             int e = 0;
             while (buscando)
             {
-                if (intento <167)
+                e = primos[intento];
+                if (intento <primos.Length)
                 {
                     int factor = phi % e;
-                    if (factor==0&&e<phi)
+                    if (factor!=0&&e<phi)
                     {
                         buscando = false;
                     }
@@ -67,7 +81,7 @@ namespace Cifrado_llave_publica
             }
             return e;
         }
-        public int EncontrarD(int e, int phi)//comprobar metodo
+        private int EncontrarD(int e, int phi)
         {
             int eaux = 0;
             int phiaux = 0;
@@ -88,7 +102,7 @@ namespace Cifrado_llave_publica
                 e2 = eaux - multiplicacione;
                 if (phi2 < 0)
                 {
-                    phi2 = phi2 % phi;//revisar si la operacione es correcta
+                    phi2 = phi2 % phi;
                 }
 
             }
